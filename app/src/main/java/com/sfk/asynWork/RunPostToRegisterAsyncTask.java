5e0e7e3fff89b669326b80303a5fe9b89d410e67
xmfsimida <xmfsimida@163.com>
2015-05-26 19:15:44 +0800

    sfk

---
+++ app/src/main/java/com/sfk/asynWork/RunPostToRegisterAsyncTask.java
@@ -0,0 +1,78 @@
+package com.sfk.asynWork;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.root.sfk.MainActivity;
+import com.example.root.sfk.NetUtils;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by root on 5/17/15.
+ */
+public class RunPostToRegisterAsyncTask extends AsyncTask {
+    private Context context;
+    private EditText et_username, et_password;
+    private TextView tv_info;
+
+    public RunPostToRegisterAsyncTask(Context context, EditText et_username,
+                                      EditText et_password, TextView tv_info) {
+        this.context = context;
+        this.et_username = et_username;
+        this.et_password = et_password;
+        this.tv_info = tv_info;
+    }
+
+    @Override
+    protected Object doInBackground(Object[] params) {
+        String result = NetUtils.loginOfPost(et_username.getText().toString(),
+                et_password.getText().toString(),
+                "http://192.168.1.116:8080/shaFaKe/customer/register!checkUserUsabilityAndRegister");
+        return result;
+    }
+
+    @Override
+    protected void onPostExecute(Object o) {
+        super.onPostExecute(o);
+        String info = (String) o;
+        try {
+            JSONObject jsonObject = new JSONObject(info);
+            int num = jsonObject.getInt("info");
+            switch (num) {
+                case 200:
+                    tv_info.setText("注册成功！");
+                    SharedPreferences spf = context.getSharedPreferences("LOGIN_STATUS", 0);
+                    SharedPreferences.Editor editor = spf.edit();
+                    editor.putString("username", jsonObject.getString("username"));
+                    editor.commit();
+                    Intent intent = new Intent(context, MainActivity.class);
+                    context.startActivity(intent);
+                    break;
+                case 400:
+                    tv_info.setText("注册失败，请填写正确的用户！");
+                    break;
+                case 404:
+                    tv_info.setText("服务器没有响应！");
+                    break;
+                default:
+                    tv_info.setText("不能连接服务器！");
+                    break;
+            }
+            Log.i("result", num + "");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        tv_info.setVisibility(View.VISIBLE);
+        new DismissViewAsynTask(tv_info).execute();
+    }
+}
+
+
