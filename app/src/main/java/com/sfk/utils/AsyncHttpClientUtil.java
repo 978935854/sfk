e59ebecf10a1ce41094abc0ed058542945321ce8
wgwgwgw <wei171635069>
2015-05-31 15:49:25 +0800

    sfk

---
+++ app/src/main/java/com/sfk/utils/AsyncHttpClientUtil.java
@@ -0,0 +1,21 @@
+package com.sfk.utils;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+/**客户端异步请求服务器工具类
+ * Created by WEI on 2015/5/20.
+ */
+public class AsyncHttpClientUtil {
+
+    private static AsyncHttpClient client=new AsyncHttpClient();
+
+    public static void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.get(url, params, responseHandler);
+    }
+
+    public static void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.post(url, params, responseHandler);
+    }
+}
