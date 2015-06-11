5a33c7a2d5ae8ed94bc8fc5f81589fffe490c462
978935854 <978935854@qq.com>
2015-06-11 17:18:42 +0800

    sfk

--- app/src/main/java/com/sfk/Constant/Constant.java
+++ app/src/main/java/com/sfk/Constant/Constant.java
@@ -4,6 +4,6 @@ public class Constant {
 
     //项目服务器的连接
 
-    public static final String projectServicePath = "http://192.168.103.1:8080/shaFaKe/";
+    public static final String projectServicePath = "http://192.168.1.121:8080/shaFaKe/";
 
 }
